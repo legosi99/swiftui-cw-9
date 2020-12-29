1e1b4b34ec4accd837e8e63ad2ab30885343301f
legosi99 <62063512+legosi99@users.noreply.github.com>
2020-12-29 22:35:44 +0300

    Commit swiftui-cw-9

---
+++ swiftui-cw-9/swiftui-cw-9/swiftui_cw_9App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_cw_9App.swift
+//  swiftui-cw-9
+//
+//  Created by Yousif on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_cw_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
